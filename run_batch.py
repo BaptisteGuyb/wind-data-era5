259156fd25c17385d7b45f0f706f1c685443e326
bguybert-ensae <baptiste.guybert@ensae.fr>
2025-10-20 13:32:33 +0000

    Update Non summer months for run_batch.py

--- run_batch.py
+++ run_batch.py
@@ -3,10 +3,12 @@ import subprocess
 
 start_year = 1976
 end_year = 1990
-dt_hours = 1  # ou 3 ou 6 selon ce que tu veux
+dt_hours = 1  # ou 3 ou 6
+
+non_summer_months <- [1,2,3,4,5,9,10,11,12]
 
 for year in range(start_year, end_year + 1):
-    for month in range(1, 13):
+    for month in non_summer_months:
         print(f"==> Running for {year}-{month:02d}")
         cmd = [
             "python", 
